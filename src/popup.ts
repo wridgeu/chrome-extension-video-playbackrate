d9a4dd2dbd4e857150b5b4daf49a7d92524b5c97
Marco Beier <marco-beier@t-online.de>
2021-11-28 17:12:00 +0100

    add some comments and refactor how args are passed with executeScript
    - https://developer.chrome.com/docs/extensions/reference/scripting/#:~:text=You%20can%20work%20around%20this%20by%20using%20the%20args%20property%3A

--- src/popup.ts
+++ src/popup.ts
@@ -2,23 +2,28 @@ import "@ui5/webcomponents/dist/Slider";
 
 let sliderComponent = (document.getElementById("sliderWebComponent") as HTMLElement);
 
+/**
+ * Listen on change of UI5 Slider WebC 
+ */
 sliderComponent.addEventListener("change", async (e) => {
 	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 	let targetSpeed = (e.target! as any).value;
-	chrome.storage.sync.set({targetSpeed});
 	chrome.scripting.executeScript({
 	  target: { tabId: (tab.id as number) },
-	  func: setVideoPlayerSpeed
+	  func: setVideoPlayerSpeed,
+	  args: [targetSpeed]
 	});
 });
   
-function setVideoPlayerSpeed(): void {
-	chrome.storage.sync.get("targetSpeed", ({ targetSpeed }) => {
-		let firstVideoElementOfPage = (document.getElementsByTagName("video")[0] as HTMLVideoElement);
-		if(firstVideoElementOfPage){
-			firstVideoElementOfPage.playbackRate = targetSpeed;
-		}
-	});
+/**
+ * TODO: maybe extract into file
+ * @param targetSpeed 
+ */
+function setVideoPlayerSpeed(targetSpeed: number): void {
+	let firstVideoElementOfPage = (document.getElementsByTagName("video")[0] as HTMLVideoElement);
+	if(firstVideoElementOfPage){
+		firstVideoElementOfPage.playbackRate = targetSpeed;
+	}
 
 	//TODO: maybe set 1 on error:  chrome.storage.sync.get({ defaultSpeed }); ...
 }
--- src/sw.ts
+++ src/sw.ts
@@ -2,5 +2,4 @@ let defaultSpeed = 1;
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ defaultSpeed });
-  console.log(`Default videoplayer speed set to ${defaultSpeed}`);
 });
