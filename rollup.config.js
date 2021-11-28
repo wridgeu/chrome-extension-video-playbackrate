bece2be9b7aee027f19a47a7cd8070c43c7f3078
Marco Beier <marco-beier@t-online.de>
2021-11-28 20:17:39 +0100

    include uglifying/minifying plugin - terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import { terser } from "rollup-plugin-terser";
 import copy from 'rollup-plugin-copy';
 
 export default {
@@ -10,6 +11,7 @@ export default {
   },
   plugins: [
     resolve(),
+    terser(),
     typescript({
       module: "ESNext"
     }),
