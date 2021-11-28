5e7cd606e0275c136ff55df965b0d437d04823de
Marco Beier <marco-beier@t-online.de>
2021-11-28 21:08:40 +0100

    minor adjustments to types & todo

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,4 +1,13 @@
-/** 
+export async function adjustPlaybackrate(targetSpeed: number){
+	let firstVideoElementOfPage = (document.getElementsByTagName("video")[0] as HTMLVideoElement)
+	if(firstVideoElementOfPage){
+		firstVideoElementOfPage.playbackRate = targetSpeed
+	}
+}
+
+/**
+ * retrieves some information about all available video tags of the current page/tab
+ * 
  * TODO:
  * function -> pageContext -> retrieve e.g. 'src' into extension context
  * build up table with video elements by number of 'src' in extension context
@@ -9,34 +18,12 @@
  * - default: first video element to be found on page
  * 
  * Example query Selector by src document.querySelector("video[src='blob:https://www.youtube.com/4125af81-c8ed-49eb-91f4-950c28676513']")
- *
- *	let videoElements = await chrome.scripting.executeScript({
- *		target: { tabId: (tab.id as number) },
- *		func: retrieveVideoElements,
- *	  });
- *
- *	function retrieveVideoElements(): any {
- *		let videoTags =  (document.getElementsByTagName("video")[0] as HTMLVideoElement).src;	
- *		console.log(videoTags)
- *		return videoTags 
- *	}
- *	console.log(videoElements);
- */
-export async function adjustPlaybackrate(targetSpeed: number){
-	let firstVideoElementOfPage = (document.getElementsByTagName("video")[0] as HTMLVideoElement)
-	if(firstVideoElementOfPage){
-		firstVideoElementOfPage.playbackRate = targetSpeed
-	}
-}
-
-/**
- * retrieves some information about all available video tags of the current page/tab
  */
 export async function retrieveVideoElements(){
-	let videoElementsCollection =  (document.getElementsByTagName("video"))	
-	const videoElements: any[] = [];
+	let videoElementsCollection =  (document.getElementsByTagName("video") as HTMLCollectionOf<HTMLVideoElement>)
+	let videoElements: { src: string }[] = [];
 	for(let element of videoElementsCollection){
-		videoElements.push({ src: element.src } )
+		videoElements.push({ src: element.src })
 	}
 	return videoElements
 }
