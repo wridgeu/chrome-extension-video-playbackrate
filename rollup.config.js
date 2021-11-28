82f761f52432e8772de094a73f1b4782abdcb12d
Marco Beier <marco-beier@t-online.de>
2021-11-28 20:24:20 +0100

    added additional config for multiple glob entry pattern

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
-import resolve from 'rollup-plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from "rollup-plugin-terser";
+import multiInput from 'rollup-plugin-multi-input';
+import resolve from 'rollup-plugin-node-resolve';
 import copy from 'rollup-plugin-copy';
 
 export default {
-  input: ['./src/options.ts', './src/popup.ts', './src/sw.ts'],
+  input: ['src/*.ts'],
   output: {
     dir: 'dist/js/',
     format: 'cjs'
@@ -12,6 +13,7 @@ export default {
   plugins: [
     resolve(),
     terser(),
+    multiInput(),
     typescript({
       module: "ESNext"
     }),
