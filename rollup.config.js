57797c60ffcb1be2a5f0b41710f5b6be4524d87c
Marco Beier <marco-beier@t-online.de>
2021-12-15 21:05:57 +0100

    rough impl. of option page

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
 import typescript from '@rollup/plugin-typescript';
+import resolve from 'rollup-plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 import multiInput from 'rollup-plugin-multi-input';
-import resolve from 'rollup-plugin-node-resolve';
 import copy from 'rollup-plugin-copy';
 
 export default {
     input: ['src/*.ts'],
     output: {
         dir: 'dist/js/',
-        format: 'cjs'
+        format: 'es'
     },
     plugins: [
         resolve(),
--- src/options.ts
+++ src/options.ts
@@ -1,10 +1,56 @@
-// let page = document.getElementById("buttonDiv");
+import '@ui5/webcomponents/dist/CheckBox';
+import '@ui5/webcomponents/dist/Select';
+import '@ui5/webcomponents/dist/Option';
+import '@ui5/webcomponents/dist/Label';
+
+/**
+ * @todo add types, refactor saving mechanism -> central save (shaking of data)
+ */
+document.addEventListener('DOMContentLoaded', async () => {
+    const defaultsEnabledCheckbox = <any>document.getElementById('defaultsEnabledCheckbox')!;
+    const defaultSpeedSelector = <any>document.getElementById('defaultSpeedSelector')!;
+
+    // @todo refactor
+    const { defaults } = await chrome.storage.sync.get('defaults');
+    defaultsEnabledCheckbox.checked = defaults?.enabled || false;
+    if (defaultsEnabledCheckbox.checked) {
+        defaultSpeedSelector.disabled = false;
+    }
+    if (defaults?.playbackrate) {
+        document.getElementById(`option-${defaults.playbackrate}`)?.setAttribute('selected', '');
+    }
+
+    defaultsEnabledCheckbox.addEventListener('change', async (event: any) => {
+        if (event.target.checked === true) {
+            defaultSpeedSelector.disabled = false;
+        } else {
+            defaultSpeedSelector.disabled = true;
+        }
+        await chrome.storage.sync.set({
+            defaults: {
+                enabled: event.target.checked,
+                playbackrate: defaultSpeedSelector.selectedOption.getInnerHTML()
+            }
+        });
+    });
+
+    defaultSpeedSelector.addEventListener('change', async (event: any) => {
+        await chrome.storage.sync.set({
+            defaults: {
+                enabled: defaultsEnabledCheckbox.checked,
+                playbackrate: defaultSpeedSelector.selectedOption.getInnerHTML()
+            }
+        });
+    });
+});
+
+// let page = <HTMLDivElement>document.getElementById("buttonDiv");
 // let selectedClassName = "current";
 // const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
 
 // // Reacts to a button click by marking the selected button and saving
 // // the selection
-// function handleButtonClick(event) {
+// function handleButtonClick(event: any) {
 //   // Remove styling from the previously selected color
 //   let current = event.target.parentElement.querySelector(
 //     `.${selectedClassName}`
@@ -20,7 +66,7 @@
 // }
 
 // // Add a button to the page for each supplied color
-// function constructOptions(buttonColors) {
+// function constructOptions(buttonColors: any) {
 //   chrome.storage.sync.get("color", (data) => {
 //     let currentColor = data.color;
 //     // For each color we were provided…
