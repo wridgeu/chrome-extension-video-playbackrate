5d7ecd165809fa74c59db0cebd0c8c33a40d55bf
Marco Beier <marco-beier@t-online.de>
2021-12-21 11:00:03 +0100

    Pre-Commit hook via husky (#16)
    * feat: pre commit-hooks via husky => prettier run
    
    https://www.npmjs.com/package/husky
    https://github.com/typicode/husky

--- src/popup.ts
+++ src/popup.ts
@@ -6,9 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
         const sliderComponent = <UI5Slider>document.getElementById('sliderWebComponent');
         const [{ id }] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-        chrome.tabs.sendMessage(<number>id, { action: ChromeMessagingRequestAction.RETRIEVE }, (response: ChromeMessagingResponse) => {
-            sliderComponent.value = response.playbackRate || 1;
-        });
+        chrome.tabs.sendMessage(
+            <number>id,
+            { action: ChromeMessagingRequestAction.RETRIEVE },
+            (response: ChromeMessagingResponse) => {
+                sliderComponent.value = response.playbackRate || 1;
+            }
+        );
 
         /**
          * Listen on change of UI5 Slider WebC
@@ -16,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
         sliderComponent.addEventListener('change', async (e): Promise<void> => {
             const targetSpeed = <number>(e.target as UI5Slider).value;
             chrome.tabs.sendMessage(<number>id, {
-                action:  ChromeMessagingRequestAction.SET,
+                action: ChromeMessagingRequestAction.SET,
                 playbackRate: targetSpeed
             });
         });
