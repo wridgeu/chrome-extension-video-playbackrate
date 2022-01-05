e13b10bc8b0f9581f27012394c89556e8c731448
Marco Beier <marco-beier@t-online.de>
2022-01-05 17:26:29 +0100

    remove unused iife

--- src/popup.ts
+++ src/popup.ts
@@ -1,35 +1,33 @@
 import { ChromeMessagingRequestAction, ChromeMessagingResponse, UI5Slider } from '../types';
 import '@ui5/webcomponents/dist/Slider';
 
-document.addEventListener('DOMContentLoaded', () => {
-    (async (): Promise<void> => {
-        const sliderComponent = <UI5Slider>document.getElementById('sliderWebComponent');
-        const [{ id }] = await chrome.tabs.query({ active: true, currentWindow: true });
+document.addEventListener('DOMContentLoaded', async () => {
+    const sliderComponent = <UI5Slider>document.getElementById('sliderWebComponent');
+    const [{ id }] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-        /**
-         * Initialize state
-         */
-        chrome.tabs.sendMessage(
-            <number>id,
-            { action: ChromeMessagingRequestAction.RETRIEVE },
-            (response: ChromeMessagingResponse) => {
-                if (!chrome.runtime.lastError && response) {
-                    sliderComponent.value = response.playbackRate;
-                } else {
-                    sliderComponent.value = 1;
-                }
+    /**
+     * Initialize state
+     */
+    chrome.tabs.sendMessage(
+        <number>id,
+        { action: ChromeMessagingRequestAction.RETRIEVE },
+        (response: ChromeMessagingResponse) => {
+            if (!chrome.runtime.lastError && response) {
+                sliderComponent.value = response.playbackRate;
+            } else {
+                sliderComponent.value = 1;
             }
-        );
+        }
+    );
 
-        /**
-         * Listen on change of UI5 Slider WebC
-         */
-        sliderComponent.addEventListener('change', async (e): Promise<void> => {
-            const targetSpeed = <number>(e.target as UI5Slider).value;
-            chrome.tabs.sendMessage(<number>id, {
-                action: ChromeMessagingRequestAction.SET,
-                playbackRate: targetSpeed
-            });
+    /**
+     * Listen on change of UI5 Slider WebC
+     */
+    sliderComponent.addEventListener('change', async (e): Promise<void> => {
+        const targetSpeed = <number>(e.target as UI5Slider).value;
+        chrome.tabs.sendMessage(<number>id, {
+            action: ChromeMessagingRequestAction.SET,
+            playbackRate: targetSpeed
         });
-    })();
+    });
 });
